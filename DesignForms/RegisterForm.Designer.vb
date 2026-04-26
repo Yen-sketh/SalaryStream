fa075e572a37b3f336f40d2f768ac276b2247428
Yen-sketh <editorin22@gmail.com>
2026-04-26 14:08:15 +0800

    Added dataGrid

--- DesignForms/RegisterForm.Designer.vb
+++ DesignForms/RegisterForm.Designer.vb
@@ -36,6 +36,13 @@ Partial Class RegisterForm
         Label5 = New Label()
         ContextMenuStrip1 = New ContextMenuStrip(components)
         TextBox1 = New TextBox()
+        DataGridView1 = New DataGridView()
+        Name = New DataGridViewTextBoxColumn()
+        Column1 = New DataGridViewTextBoxColumn()
+        Column2 = New DataGridViewTextBoxColumn()
+        Column3 = New DataGridViewTextBoxColumn()
+        Column4 = New DataGridViewTextBoxColumn()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Label1
@@ -141,11 +148,57 @@ Partial Class RegisterForm
         TextBox1.Size = New Size(915, 35)
         TextBox1.TabIndex = 13
         ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Name, Column1, Column2, Column3, Column4})
+        DataGridView1.Location = New Point(25, 317)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 72
+        DataGridView1.Size = New Size(945, 439)
+        DataGridView1.TabIndex = 14
+        ' 
+        ' Name
+        ' 
+        Name.HeaderText = "Name"
+        Name.MinimumWidth = 9
+        Name.Name = "Name"
+        Name.Width = 175
+        ' 
+        ' Column1
+        ' 
+        Column1.HeaderText = "Position"
+        Column1.MinimumWidth = 9
+        Column1.Name = "Column1"
+        Column1.Width = 175
+        ' 
+        ' Column2
+        ' 
+        Column2.HeaderText = "Daily Rate"
+        Column2.MinimumWidth = 9
+        Column2.Name = "Column2"
+        Column2.Width = 175
+        ' 
+        ' Column3
+        ' 
+        Column3.HeaderText = "Attendance (days worked)"
+        Column3.MinimumWidth = 9
+        Column3.Name = "Column3"
+        Column3.Width = 175
+        ' 
+        ' Column4
+        ' 
+        Column4.HeaderText = "Overtime (in hrs)"
+        Column4.MinimumWidth = 9
+        Column4.Name = "Column4"
+        Column4.Width = 175
+        ' 
         ' RegisterForm
         ' 
         AutoScaleDimensions = New SizeF(12F, 30F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(982, 768)
+        Controls.Add(DataGridView1)
         Controls.Add(TextBox1)
         Controls.Add(Label5)
         Controls.Add(txtboxOvertime)
@@ -160,6 +213,7 @@ Partial Class RegisterForm
         Controls.Add(Label1)
         Name = "RegisterForm"
         Text = "Register Employee"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -177,4 +231,10 @@ Partial Class RegisterForm
     Friend WithEvents Label5 As Label
     Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Name As DataGridViewTextBoxColumn
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
 End Class
