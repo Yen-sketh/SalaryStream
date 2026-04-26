6e0b8dc8791d062cfb8db4c6839ef894f40c37f3
Yen-sketh <editorin22@gmail.com>
2026-04-26 16:56:51 +0800

    removed form1

--- DesignForms/Form1.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class Form1
-    Private Sub CheckedListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
-
-    End Sub
-End Class
--- DesignForms/LoginForm.Designer.vb
+++ DesignForms/LoginForm.Designer.vb
@@ -35,20 +35,18 @@ Partial Class LoginForm
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(162, 23)
-        Label1.Margin = New Padding(2, 0, 2, 0)
+        Label1.Location = New Point(243, 34)
         Label1.Name = "Label1"
-        Label1.Size = New Size(0, 20)
+        Label1.Size = New Size(0, 30)
         Label1.TabIndex = 0
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
         Label2.Font = New Font("Segoe UI", 20F)
-        Label2.Location = New Point(8, 6)
-        Label2.Margin = New Padding(2, 0, 2, 0)
+        Label2.Location = New Point(12, 9)
         Label2.Name = "Label2"
-        Label2.Size = New Size(106, 46)
+        Label2.Size = New Size(147, 62)
         Label2.TabIndex = 1
         Label2.Text = "Email:"
         ' 
@@ -56,64 +54,58 @@ Partial Class LoginForm
         ' 
         Label3.AutoSize = True
         Label3.Font = New Font("Segoe UI", 20F)
-        Label3.Location = New Point(8, 47)
-        Label3.Margin = New Padding(2, 0, 2, 0)
+        Label3.Location = New Point(12, 70)
         Label3.Name = "Label3"
-        Label3.Size = New Size(167, 46)
+        Label3.Size = New Size(231, 62)
         Label3.TabIndex = 2
         Label3.Text = "Password:"
         ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New Point(110, 20)
-        TextBox1.Margin = New Padding(2)
+        TextBox1.Location = New Point(165, 30)
         TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(118, 27)
+        TextBox1.Size = New Size(175, 35)
         TextBox1.TabIndex = 3
         ' 
         ' TextBox2
         ' 
-        TextBox2.Location = New Point(162, 63)
-        TextBox2.Margin = New Padding(2)
+        TextBox2.Location = New Point(243, 94)
         TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(118, 27)
+        TextBox2.Size = New Size(175, 35)
         TextBox2.TabIndex = 4
         ' 
         ' btnClear
         ' 
-        btnClear.Location = New Point(19, 93)
-        btnClear.Margin = New Padding(2)
+        btnClear.Location = New Point(28, 140)
         btnClear.Name = "btnClear"
-        btnClear.Size = New Size(87, 27)
+        btnClear.Size = New Size(130, 40)
         btnClear.TabIndex = 5
         btnClear.Text = "Clear"
         btnClear.UseVisualStyleBackColor = True
         ' 
         ' btnRegister
         ' 
-        btnRegister.Location = New Point(110, 93)
-        btnRegister.Margin = New Padding(2)
+        btnRegister.Location = New Point(165, 140)
         btnRegister.Name = "btnRegister"
-        btnRegister.Size = New Size(87, 27)
+        btnRegister.Size = New Size(130, 40)
         btnRegister.TabIndex = 6
         btnRegister.Text = "Register"
         btnRegister.UseVisualStyleBackColor = True
         ' 
         ' btnLogin
         ' 
-        btnLogin.Location = New Point(201, 93)
-        btnLogin.Margin = New Padding(2)
+        btnLogin.Location = New Point(302, 140)
         btnLogin.Name = "btnLogin"
-        btnLogin.Size = New Size(87, 27)
+        btnLogin.Size = New Size(130, 40)
         btnLogin.TabIndex = 7
         btnLogin.Text = "Login"
         btnLogin.UseVisualStyleBackColor = True
         ' 
         ' LoginForm
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(12F, 30F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(319, 137)
+        ClientSize = New Size(478, 206)
         Controls.Add(btnLogin)
         Controls.Add(btnRegister)
         Controls.Add(btnClear)
@@ -122,7 +114,6 @@ Partial Class LoginForm
         Controls.Add(Label3)
         Controls.Add(Label2)
         Controls.Add(Label1)
-        Margin = New Padding(2)
         Name = "LoginForm"
         StartPosition = FormStartPosition.CenterScreen
         Text = "Salary Stream"
