ef4d15cb9bd4a05a4f5e406ac5249cd10de7901d
Yen-sketh <editorin22@gmail.com>
2026-04-26 14:18:42 +0800

    Fixed dataGrigBuf

--- DesignForms/LoginForm.vb
+++ DesignForms/LoginForm.vb
@@ -1,6 +1,4 @@
 ﻿Public Class LoginForm
-
-
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
--- DesignForms/RegisterForm.Designer.vb
+++ DesignForms/RegisterForm.Designer.vb
@@ -37,11 +37,11 @@ Partial Class RegisterForm
         ContextMenuStrip1 = New ContextMenuStrip(components)
         TextBox1 = New TextBox()
         DataGridView1 = New DataGridView()
-        Name = New DataGridViewTextBoxColumn()
         Column1 = New DataGridViewTextBoxColumn()
         Column2 = New DataGridViewTextBoxColumn()
         Column3 = New DataGridViewTextBoxColumn()
         Column4 = New DataGridViewTextBoxColumn()
+        Column5 = New DataGridViewTextBoxColumn()
         CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
@@ -151,47 +151,47 @@ Partial Class RegisterForm
         ' DataGridView1
         ' 
         DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Name, Column1, Column2, Column3, Column4})
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Column1, Column2, Column3, Column4, Column5})
         DataGridView1.Location = New Point(25, 317)
         DataGridView1.Name = "DataGridView1"
         DataGridView1.RowHeadersWidth = 72
         DataGridView1.Size = New Size(945, 439)
         DataGridView1.TabIndex = 14
         ' 
-        ' Name
-        ' 
-        Name.HeaderText = "Name"
-        Name.MinimumWidth = 9
-        Name.Name = "Name"
-        Name.Width = 175
-        ' 
         ' Column1
         ' 
-        Column1.HeaderText = "Position"
+        Column1.HeaderText = "Name"
         Column1.MinimumWidth = 9
         Column1.Name = "Column1"
         Column1.Width = 175
         ' 
         ' Column2
         ' 
-        Column2.HeaderText = "Daily Rate"
+        Column2.HeaderText = "Position"
         Column2.MinimumWidth = 9
         Column2.Name = "Column2"
         Column2.Width = 175
         ' 
         ' Column3
         ' 
-        Column3.HeaderText = "Attendance (days worked)"
+        Column3.HeaderText = "Daily Rate"
         Column3.MinimumWidth = 9
         Column3.Name = "Column3"
         Column3.Width = 175
         ' 
         ' Column4
         ' 
-        Column4.HeaderText = "Overtime (in hrs)"
+        Column4.HeaderText = "Attendance (days worked)"
         Column4.MinimumWidth = 9
         Column4.Name = "Column4"
         Column4.Width = 175
+        ' 
+        ' Column5
+        ' 
+        Column5.HeaderText = "Overtime (in hrs)"
+        Column5.MinimumWidth = 9
+        Column5.Name = "Column5"
+        Column5.Width = 175
         ' 
         ' RegisterForm
         ' 
@@ -232,9 +232,9 @@ Partial Class RegisterForm
     Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents Name As DataGridViewTextBoxColumn
     Friend WithEvents Column1 As DataGridViewTextBoxColumn
     Friend WithEvents Column2 As DataGridViewTextBoxColumn
     Friend WithEvents Column3 As DataGridViewTextBoxColumn
     Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
 End Class
