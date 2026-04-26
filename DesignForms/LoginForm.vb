21594d061806255241e8f28183364824a2f793bf
Yen-sketh <editorin22@gmail.com>
2026-04-26 16:31:37 +0800

    Deleted LogIn

--- DesignForms/LoginForm.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class LoginForm
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs)
-
-    End Sub
-End Class
