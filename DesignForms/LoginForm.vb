8d20188219469e041bc9f770b3cc3c9248545f8b
Yen-sketh <editorin22@gmail.com>
2026-04-26 22:52:06 +0800

    Removed regForm

--- DesignForms/LoginForm.vb
+++ DesignForms/LoginForm.vb
@@ -26,4 +26,8 @@
     Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
 
     End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
 End Class
