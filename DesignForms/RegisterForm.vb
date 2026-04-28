71e08cd14ceac791a30594d5683820da9886fa5f
Yen-sketh <editorin22@gmail.com>
2026-04-28 14:12:35 +0800

    fixing

--- DesignForms/RegisterForm.vb
+++ DesignForms/RegisterForm.vb
@@ -69,14 +69,16 @@
 
     End Sub
 
-    Dim index As Integer = DataGridView1.CurrentRow.Index
-    Dim name As String = DataGridView1.Rows(index).Cells(0).Value
-    Dim position As String = DataGridView1.Rows(index).Cells(1).Value
-    Dim dailyRate As Double = DataGridView1.Rows(index).Cells(2).Value
-    Dim attendace As Integer = DataGridView1.Rows(index).Cells(3).Value
-    Dim overtime As Integer = DataGridView1.Rows(index).Cells(4).Value
+    Private Sub txtboxSearchEmployee_TextChanged(sender As Object, e As EventArgs) Handles txtboxSearchEmployee.TextChanged
+        If checkboxEmployee.Checked Then
+            search_employee()
 
+        ElseIf checkboxEmployee.Checked = False Then
 
-
+            For i = 0 To DataGridView1.Rows.Count - 1
+                DataGridView1.Rows(i).Visible = True
+            Next
+        End If
+    End Sub
 
 End Class
