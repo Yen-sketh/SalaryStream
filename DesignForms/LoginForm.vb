46bd5c2be03a89724fe783d7020f95cff9daa9fe
Francis Antonio <antoniofrancis222@gmail.com>
2026-05-03 18:18:01 +0800

    done

--- DesignForms/LoginForm.vb
+++ DesignForms/LoginForm.vb
@@ -10,15 +10,17 @@
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
 
         Dim username As String = "Admin"
-        Dim password As String = "00000000"
+        Dim password As String = "6789"
 
         If TextBox1.Text = username And TextBox2.Text = password Then
             MessageBox.Show("Login Successful!", "Payroll System", MessageBoxButtons.OK, MessageBoxIcon.Information)
 
-            Dim main As New MainForm
+            'mag oopen sya ng bagong form after mag log in
+            Dim main As New RegisterForm
 
             main.Show()
             Me.Hide()
+
         Else
             MessageBox.Show("Invalid Username or Password!", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error)
 
